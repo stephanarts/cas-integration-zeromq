39d14d5a6372f1e04441ac8663df7da131324904
Stephan Arts <stephan@xfce.org>
2014-05-08 15:27:15 +0200

    Initial Commit
     - Copy Checkstyle Rules from http://github.com/Jasig/cas
     - Copy header.txt and header-definitions.xml from http://github.com/Jasig/cas
     - Commit rudimentary TicketRegistry

---
+++ src/main/java/com/github/stephanarts/cas/ticket/registry/Provider.java
@@ -0,0 +1,216 @@
+/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.github.stephanarts.cas.ticket.registry;
+
+import java.util.HashMap;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.json.JSONObject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.jasig.cas.ticket.Ticket;
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
+//import org.zeromq.ZMQ.PollItem;
+import org.zeromq.ZMQ.Poller;
+
+/**
+ * Provider Class.
+ */
+class Provider extends Thread {
+
+    /**
+     * Logging Class.
+     */
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * ZMQ Context.
+     */
+    private final Context context;
+
+    /**
+     * ZMQ Socket.
+     */
+    private final Socket  socket;
+
+    private final HashMap<Integer, Ticket> map;
+
+    /**
+     *  Create a RegistryProvider.
+     *
+     *  @param context    ZeroMQ Context
+     *  @param bindUri    BindURI
+     */
+    public Provider(final Context context, final String bindUri) {
+
+        this.context = ZMQ.context(1);
+
+        this.socket = this.context.socket(ZMQ.ROUTER);
+        this.socket.bind(bindUri);
+        this.setName("ZMQProviderThread-1");
+
+        this.map = new HashMap<Integer, Ticket>();
+
+    }
+
+    /**
+     * The main provider loop.
+     */
+    public void run() {
+        ZMsg message;
+        Poller items = new Poller(1);
+        items.register(socket, Poller.POLLIN);
+
+        JSONObject request;
+        JSONObject requestParams;
+        String     requestId;
+        String     requestMethod;
+        String     ticketId;
+        String     serializedTicket;
+
+        JSONObject response;
+        JSONObject responseResult;
+
+        while(!Thread.currentThread().isInterrupted()) {
+            // poll and memorize multipart detection
+            items.poll();
+
+            if(items.pollin(0)) {
+                message = ZMsg.recvMsg(socket);
+                logger.debug(new String(message.getLast().getData()));
+                try {
+                    request = new JSONObject(new String(message.getLast().getData()));
+                    requestId     = request.getString("id");
+                    requestParams = request.getJSONObject("params");
+                    requestMethod = request.getString("method");
+
+                    response = new JSONObject();
+                    responseResult = new JSONObject();
+                    response.put("json-rpc", "2.0");
+                    response.put("id", requestId);
+
+                    try {
+
+                        if(requestMethod.equals("del")) {
+                            logger.debug("Delete Ticket");
+
+                            ticketId = requestParams.getString("ticket-id");
+
+                            if(!this.map.containsKey(ticket.hashCode())) {
+                                logger.warn("Missing Key {}", ticketId);
+                            }
+
+                            this.map.remove(ticketId.hashCode());
+                        }
+                        if(requestMethod.equals("update")) {
+                            logger.debug("Update Ticket");
+
+                            ticketId = requestParams.getString("ticket-id");
+                            serializedTicket = requestParams.getString("ticket");
+
+                            ByteArrayInputStream bi = new ByteArrayInputStream(DatatypeConverter.parseBase64Binary(serializedTicket));
+                            ObjectInputStream si = new ObjectInputStream(bi);
+
+                            Ticket ticket =(Ticket) si.readObject();
+
+                            if(!this.map.containsKey(ticket.hashCode())) {
+                                logger.warn("Missing Key {}", ticketId);
+                            }
+
+                            this.map.put(ticketId.hashCode(), ticket);
+
+                            logger.debug("Ticket-ID '{}'", ticketId);
+
+                        }
+                        if(requestMethod.equals("add")) {
+                            logger.debug("Add Ticket");
+                            ticketId = requestParams.getString("ticket-id");
+                            serializedTicket = requestParams.getString("ticket");
+
+                            ByteArrayInputStream bi = new ByteArrayInputStream(DatatypeConverter.parseBase64Binary(serializedTicket));
+                            ObjectInputStream si = new ObjectInputStream(bi);
+
+                            Ticket ticket =(Ticket) si.readObject();
+                            if(this.map.containsKey(ticket.hashCode())) {
+                                logger.error("Duplicate Key {}", ticketId);
+                            } else {
+                                this.map.put(ticketId.hashCode(), ticket);
+                            }
+
+                            logger.debug("Ticket-ID '{}'", ticketId);
+
+
+                        }
+                        if(requestMethod.equals("get")) {
+                            logger.debug("GET");
+
+                            ticketId = requestParams.getString("ticket-id");
+                           
+                            Ticket t = this.map.get(ticketId.hashCode());
+                            byte[] serializedTicketArray;
+
+                            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+                            ObjectOutputStream so = new ObjectOutputStream(bo);
+                            so.writeObject(t);
+                            so.flush();
+                            serializedTicketArray = bo.toByteArray();
+
+                            responseResult.put("ticket-id", ticketId);
+                            responseResult.put("ticket", DatatypeConverter.printBase64Binary(serializedTicketArray));
+                        }
+
+                        response.put("result", responseResult);
+                    } catch(final Exception e) {
+                        response.put("error", responseResult);
+                    }
+
+                    message.removeLast();
+                    message.addString(response.toString());
+                    message.send(socket);
+
+                } catch(final Exception e) {
+                    logger.error(e.getMessage());
+                }
+            }
+            /*
+            try {
+                logger.debug("Provider Run");
+                Thread.sleep(1000);
+            } catch(InterruptedException e) {
+            }
+            */
+        }
+        this.socket.close();
+        this.context.term();
+    }
+}
---
+++ src/main/java/com/github/stephanarts/cas/ticket/registry/ZMQTicketRegistry.java
@@ -0,0 +1,378 @@
+/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.stephanarts.cas.ticket.registry;
+
+import java.util.Collection;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+
+import javax.xml.bind.DatatypeConverter;
+
+import javax.validation.constraints.Min;
+
+import org.jasig.cas.ticket.Ticket;
+//import org.jasig.cas.ticket.ServiceTicket;
+//import org.jasig.cas.ticket.TicketGrantingTicket;
+import org.jasig.cas.ticket.registry.AbstractDistributedTicketRegistry;
+import org.springframework.beans.factory.DisposableBean;
+
+import org.json.JSONObject;
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.PollItem;
+import org.zeromq.ZMQ.Poller;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+import org.zeromq.ZMsg;
+
+
+
+/**
+* Key-value ticket registry implementation that stores tickets in memcached keyed on the ticket ID.
+*
+* @author Stephan Arts
+*/
+public final class ZMQTicketRegistry extends AbstractDistributedTicketRegistry implements DisposableBean {
+
+    /**
+     * TGT cache entry timeout in seconds.
+     */
+    @Min(0)
+    private final int tgtTimeout;
+
+    /**
+     * ST cache entry timeout in seconds.
+     */
+    @Min(0)
+    private final int stTimeout;
+
+
+    /**
+     * ZMQ Context.
+     */
+    private final Context context;
+
+    private final Provider provider;
+
+    private final String[] hostnames;
+
+
+    private final int requestTimeout = 1500; // msecs, (> 1000!)
+
+
+    /**
+     * Creates a new instance that stores tickets in the given ZMQ Registry-Providers.
+     *
+     * @param providers                         Array of providers to connect to
+     * @param bindUri                           URI to bind the RegistryProvider on
+     * @param ticketGrantingTicketTimeOut       Timeout
+     * @param serviceTicketTimeOut              Timeout
+     */
+    public ZMQTicketRegistry(
+                final String[] providers,
+                final String bindUri,
+                final int ticketGrantingTicketTimeOut,
+                final int serviceTicketTimeOut) {
+        this.tgtTimeout = ticketGrantingTicketTimeOut;
+        this.stTimeout = serviceTicketTimeOut;
+
+        this.context = ZMQ.context(1);
+
+        this.provider = new Provider(this.context, bindUri);
+
+        this.provider.start();
+
+        this.hostnames = providers;
+/*
+        Socket socket;
+        String hostname;
+
+        for(int i = 0; i < this.hostnames.length; ++i)
+        {
+            hostname = this.hostnames[i];
+
+            socket = this.context.socket(ZMQ.REQ);
+            socket.connect(hostname);
+            socket.send("{}",0);
+            socket.close();
+        }
+*/
+    }
+
+    /**
+     * Update a ticket in the ticketregistry.
+     *
+     * @param ticket       Ticket-object to update in Registry
+     */
+    protected void updateTicket(final Ticket ticket) {
+        logger.debug("Updating ticket {}", ticket);
+
+        String hostname;
+        byte[] serializedTicket;
+        JSONObject obj = new JSONObject();
+        JSONObject params = new JSONObject();
+
+        Socket socket;
+
+        for(int i = 0; i < this.hostnames.length; ++i) {
+
+            hostname = this.hostnames[i];
+            logger.debug("Updating to {}", hostname);
+            try {
+                ByteArrayOutputStream bo = new ByteArrayOutputStream();
+                ObjectOutputStream so = new ObjectOutputStream(bo);
+                so.writeObject(ticket);
+                so.flush();
+                serializedTicket = bo.toByteArray();
+
+                obj.put("json-rpc", "2.0");
+                obj.put("method", "update");
+                obj.put("params", params);
+
+                params.put("ticket-id", ticket.getId());
+                params.put("ticket", DatatypeConverter.printBase64Binary(serializedTicket));
+
+                socket = this.context.socket(ZMQ.REQ);
+                socket.connect(hostname);
+                socket.send(obj.toString(), 0);
+                socket.close();
+            } catch (final Exception e) {
+                logger.error(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Add a ticket to the ticketregistry.
+     *
+     * @param ticket       Ticket-object to add to Registry
+     */
+    public void addTicket(final Ticket ticket) {
+        logger.debug("Adding ticket {}", ticket);
+
+        String hostname;
+        byte[] serializedTicket;
+        JSONObject request = new JSONObject();
+        JSONObject requestParams = new JSONObject();
+        JSONObject response;
+        JSONObject result;
+
+        Socket socket;
+
+        request.put("json-rpc", "2.0");
+        request.put("id", "41");
+        request.put("method", "add");
+        request.put("params", requestParams);
+
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            ObjectOutputStream so = new ObjectOutputStream(bo);
+            so.writeObject(ticket);
+            so.flush();
+            serializedTicket = bo.toByteArray();
+        } catch (final Exception e) {
+            return;
+        }
+
+        requestParams.put("ticket-id", ticket.getId());
+        requestParams.put("ticket", DatatypeConverter.printBase64Binary(serializedTicket));
+
+        for(int i = 0; i < this.hostnames.length; ++i) {
+            hostname = this.hostnames[i];
+            logger.debug("Adding to {}", hostname);
+            try {
+                socket = this.context.socket(ZMQ.REQ);
+                socket.connect(hostname);
+                socket.send(request.toString(), 0);
+
+                PollItem[] items = {new PollItem(socket, Poller.POLLIN)};
+                int rc = ZMQ.poll(items, this.requestTimeout);
+                if(rc == -1) {
+                    break;
+                }
+
+                if(items[0].isReadable()) {
+                    // We got a reply from the server, must match sequence
+                    ZMsg message = ZMsg.recvMsg(socket);
+                    response = new JSONObject(new String(message.getLast().getData()));
+                    result = response .getJSONObject("result");
+                    socket.close();
+                    return;
+                } else {
+                    logger.debug("Failed to get reply from {}", hostname);
+                    socket.close();
+                }
+            } catch (final Exception e) {
+                logger.error(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Delete a ticket from the ticketregistry.
+     *
+     * @param ticketId       Ticket-object to delete from Registry
+     *
+     * @return     true/false ???
+     */
+    public boolean deleteTicket(final String ticketId) {
+        logger.debug("Deleting ticket {}", ticketId);
+
+        request.put("json-rpc", "2.0");
+        request.put("id", "41");
+        request.put("method", "delete");
+        request.put("params", requestParams);
+        requestParams.put("ticket-id", ticketId);
+
+        for(int i = 0; i < this.hostnames.length; ++i) {
+
+            hostname = this.hostnames[i];
+            logger.debug("Deleting from {}", hostname);
+            try {
+                socket = this.context.socket(ZMQ.REQ);
+                socket.connect(hostname);
+                socket.send(request.toString(), 0);
+
+                PollItem[] items = {new PollItem(socket, Poller.POLLIN)};
+                int rc = ZMQ.poll(items, this.requestTimeout);
+                if(rc == -1) {
+                    break;
+                }
+
+                if(items[0].isReadable()) {
+                    // We got a reply from the server, must match sequence
+                    ZMsg message = ZMsg.recvMsg(socket);
+                    response = new JSONObject(new String(message.getLast().getData()));
+                    result = response.getJSONObject("result");
+                } else {
+                    logger.debug("Failed to get reply from {}", hostname);
+                    socket.close();
+                }
+            } catch (final Exception e) {
+                logger.debug(e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get a ticket from the ticketregistry.
+     *
+     * @param ticketId       id of ticket-object to get from Registry
+     *
+     * @return               Ticket object
+     */
+    public Ticket getTicket(final String ticketId) {
+        logger.debug("Get Ticket {}", ticketId);
+
+        String hostname;
+        JSONObject request = new JSONObject();
+        JSONObject requestParams = new JSONObject();
+        JSONObject response;
+        JSONObject result;
+
+        Socket socket;
+
+        request.put("json-rpc", "2.0");
+        request.put("id", "41");
+        request.put("method", "get");
+        request.put("params", requestParams);
+        requestParams.put("ticket-id", ticketId);
+
+        for(int i = 0; i < this.hostnames.length; ++i) {
+
+            hostname = this.hostnames[i];
+            logger.debug("Getting from {}", hostname);
+            try {
+                socket = this.context.socket(ZMQ.REQ);
+                socket.connect(hostname);
+                socket.send(request.toString(), 0);
+
+                PollItem[] items = {new PollItem(socket, Poller.POLLIN)};
+                int rc = ZMQ.poll(items, this.requestTimeout);
+                if(rc == -1) {
+                    break;
+                }
+
+                if(items[0].isReadable()) {
+                    // We got a reply from the server, must match sequence
+                    ZMsg message = ZMsg.recvMsg(socket);
+                    response = new JSONObject(new String(message.getLast().getData()));
+                    result = response.getJSONObject("result");
+
+                    String serializedTicket = result.getString("ticket");
+                    ByteArrayInputStream bi = new ByteArrayInputStream(DatatypeConverter.parseBase64Binary(serializedTicket));
+                    ObjectInputStream si = new ObjectInputStream(bi);
+
+                    Ticket ticket = (Ticket) si.readObject();
+                    socket.close();
+                    if(ticket != null) {
+                      logger.debug("Get Ticket: {}", ticketId);
+                    } else {
+                      logger.debug("Failed to get Ticket: {}", ticketId);
+                    }
+                    if(ticket != null) {
+                        return ticket;
+                    }
+                } else {
+                    logger.debug("Failed to get reply from {}", hostname);
+                    socket.close();
+                }
+            } catch (final Exception e) {
+                logger.debug(e.getMessage());
+            }
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     */
+    @Override
+    public void destroy() throws Exception {
+        this.provider.stop();
+        return;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * This operation is not supported.
+     *
+     * @throws UnsupportedOperationException if you try and call this operation.
+     */
+    @Override
+    public Collection<Ticket> getTickets() {
+        throw new UnsupportedOperationException("GetTickets not supported.");
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     */
+    @Override
+    protected boolean needsCallback() {
+        return true;
+    }
+
+}
