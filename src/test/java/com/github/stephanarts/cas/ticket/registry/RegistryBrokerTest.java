8db4e7c813aa9c53df6e77271da437d38afc75b2
Stephan Arts <stephan@xfce.org>
2015-02-26 15:04:56 +0100

    test bootstrapping with more then one ticket

--- src/test/java/com/github/stephanarts/cas/ticket/registry/RegistryBrokerTest.java
+++ src/test/java/com/github/stephanarts/cas/ticket/registry/RegistryBrokerTest.java
@@ -58,7 +58,7 @@ public class RegistryBrokerTest
     }
 
     @Test
-    public void testBootstrapSuccess() throws Exception {
+    public void testBootstrapSingleTicketSuccess() throws Exception {
         final String ticketId = "ST-1234567890ABCDEFGHIJKL-crud";
         final ServiceTicket ticket = mock(ServiceTicket.class, withSettings().serializable());
         when(ticket.getId()).thenReturn(ticketId);
@@ -101,6 +101,63 @@ public class RegistryBrokerTest
 
         Assert.assertNotNull(ticketFromRegistry);
         Assert.assertEquals(ticketId, ticketFromRegistry.getId());
+    }
+
+    @Test
+    public void testBootstrapMultipleTicketsSuccess() throws Exception {
+        final String ticketId1 = "ST-1234567890ABCDEFGHIJKL-crud1";
+        final String ticketId2 = "ST-1234567890ABCDEFGHIJKL-crud2";
+        final ServiceTicket ticket1 = mock(ServiceTicket.class, withSettings().serializable());
+        final ServiceTicket ticket2 = mock(ServiceTicket.class, withSettings().serializable());
+
+        ServiceTicket ticketFromRegistry1 = null;
+
+        when(ticket1.getId()).thenReturn(ticketId1);
+        when(ticket2.getId()).thenReturn(ticketId2);
+
+        String[] addresses = {"tcp://localhost:4440","tcp://localhost:4441"};
+
+        ZMQProvider provider0 = new ZMQProvider(addresses[0], "primary");
+        ZMQProvider provider1 = new ZMQProvider(addresses[1], "secondary");
+        RegistryClient populator = new RegistryClient(addresses[1]);
+        RegistryClient checker = new RegistryClient(addresses[0]);
+        RegistryBroker broker = null;
+
+        provider0.start();
+        provider1.start();
+
+        populator.addTicket(ticket1);
+        populator.addTicket(ticket2);
+
+
+        /*
+         * If bootstrapping is successfull,
+         * checker returns the ticket we've added
+         */
+        broker = new RegistryBroker(
+            addresses,
+            1500,
+            null,
+            "primary");
+
+        try {
+            broker.bootstrap();
+        } catch (final BootstrapException e) {
+            provider0.interrupt();
+            provider1.interrupt();
+        }
+
+        ticketFromRegistry1 = (ServiceTicket) checker.getTicket(ticketId1);
+        ticketFromRegistry2 = (ServiceTicket) checker.getTicket(ticketId2);
+
+        provider0.interrupt();
+        provider1.interrupt();
+
+        Assert.assertNotNull(ticketFromRegistry1);
+        Assert.assertEquals(ticketId1, ticketFromRegistry1.getId());
+
+        Assert.assertNotNull(ticketFromRegistry2);
+        Assert.assertEquals(ticketId2, ticketFromRegistry2.getId());
     }
 
     @Test
