9ec972bbca6fa9979a4202f817dc74d5b8e3236f
Stephan Arts <stephan@xfce.org>
2015-02-26 16:23:09 +0100

    use quote()

--- src/main/java/com/github/stephanarts/cas/ticket/registry/provider/GetTicketsMethod.java
+++ src/main/java/com/github/stephanarts/cas/ticket/registry/provider/GetTicketsMethod.java
@@ -88,7 +88,7 @@ final class GetTicketsMethod implements IMethod {
                 throw new JSONRPCException(-32500, "Error extracting Ticket");
             }
 
-            tickets.put(JSONObject.escape(DatatypeConverter.printBase64Binary(serializedTicketArray)));
+            tickets.put(JSONObject.quote(DatatypeConverter.printBase64Binary(serializedTicketArray)));
         }
 
         logger.debug("GetTickets: "+tickets.length());
